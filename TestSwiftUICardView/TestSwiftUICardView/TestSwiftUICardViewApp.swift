6807f60527aa8e534f281d45561b866d4fe56836
Asad <asad.mobiledev@gmail.com>
2024-12-29 16:37:21 +0500

    Project Setup

---
+++ TestSwiftUICardView/TestSwiftUICardView/TestSwiftUICardViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestSwiftUICardViewApp.swift
+//  TestSwiftUICardView
+//
+//  Created by Asad Mehmood on 29/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct TestSwiftUICardViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
