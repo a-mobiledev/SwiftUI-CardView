8b7dd42038a160c81f71e758fb03b0acaceb0b0e
Asad <asad.mobiledev@gmail.com>
2024-12-29 21:32:08 +0500

    designed a simple View candidate for card view as in List

--- TestSwiftUICardView/TestSwiftUICardView/ContentView.swift
+++ TestSwiftUICardView/TestSwiftUICardView/ContentView.swift
@@ -9,11 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        VStack(alignment: .leading) {
+            Text("Asad Mehmood")
+                .font(.headline)
+            Text("This product have too much comfort and easy to use.")
+                .font(.body)
+            Label("\(0)", systemImage: "hand.thumbsup.fill")
+                .font(.caption)
         }
         .padding()
     }
